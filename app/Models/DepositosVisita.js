7314a6dc52e93367079dc2961ea83e52a422696f
evertonportela <evertonportela@outlook.com>
2020-10-09 15:21:41 -0300

    Update - Edição do Model DepositosVisitas

--- app/Models/DepositosVisita.js
+++ app/Models/DepositosVisita.js
@@ -4,6 +4,17 @@
 const Model = use('Model')
 
 class DepositosVisita extends Model {
+    static boot() {
+        super.boot()
+        this.addTrait('NoTimestamp')
+        this.addHook('beforeCreate', (DepositosVisita) => {
+            DepositosVisita.is_current_owner = true
+        })
+    }
+
+    static get primaryKey() {
+        return 'depositosvisitas_id'
+    }
 }
 
 module.exports = DepositosVisita
